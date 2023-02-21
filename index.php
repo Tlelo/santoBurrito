b8879f8b7861dacb8fc73e46525c9bf0b24b2b93
Carlos Tlelo Barcena <carlos.tielo@coppel.com>
2023-02-21 16:38:07 -0700

    agregamos los primeros archivos

---
+++ index.php
@@ -0,0 +1,107 @@
+<?php include ('include/header.php');?>
+
+    <div class="container_title">
+        <div class="Luchador">
+            <div class="imgLuchador">
+                <img src="img/logoLuchador_2.png" alt="Logotipo luchador">
+            </div>
+
+            <div class="title_luchador">
+                <h1>¡Santo Burrito!</h1>
+            </div>                        
+        </div>
+    </div>
+        
+<div class="container">
+    <div class="card-group gap-4">
+        <div class="card">
+            <img src="img/menu/machaca.jpg" class="card-img-top" alt="Machaca">
+        <div class="card-body">
+            <h2 class="card-title">Burrito de Machaca</h2>
+        <p class="card-text">Orden de 3 Burritos de machaca.<br>
+                <strong>Contiene:</strong>
+                <br>-Papas. 
+                <br>-Jitomate.
+                <br>-Frijoles puercos.        
+                <br>-Queso Chihuahua.
+                <br>-Macaha casera.
+                <br>-Condimentos.
+                <br>-Tortilla de harina.
+                <br>
+                <br>
+                <br>
+            </p>
+            <p class="card-text"><strong>Precio: $40</strong></p>
+                <div class="d-grid gap-1">
+                    <button class="btn btn-success" type="button">Ordenar</button>
+                </div>
+        </div>
+  </div>
+        <div class="card">
+            <img src="img/menu/chorizo.jpg" class="card-img-top" alt="Machaca">
+        <div class="card-body">
+            <h2 class="card-title">Burrito de Chorizo con papas</h2>
+        <p class="card-text">Orden de 3 Burritos de Chorizo con papa.<br>
+                <strong>Contiene:</strong>
+                <br>-Papas. 
+                <br>-Chorizo.
+                <br>-Frijoles puercos.        
+                <br>-Queso Chihuahua.
+                <br>-Condimentos.
+                <br>-Tortilla de harina.
+                <br>
+                <br>
+            </p>
+            <p class="card-text"><strong>Precio: $40</strong></p>
+                <div class="d-grid gap-1">
+                    <button class="btn btn-success" type="button">Ordenar</button>
+                </div>
+        </div>
+    
+  </div>
+  <div class="card">
+            <img src="img/menu/mixto.jpg" class="card-img-top" alt="Machaca">
+        <div class="card-body">
+            <h2 class="card-title">Burrito Mixto</h2>
+            <p class="card-text">Orden de 2 Burritos de machaca y 1 de Chorizo con papas.<br>
+                <strong>Contiene:</strong>
+                <br>-Papas. 
+                <br>-Jitomate.
+                <br>-Frijoles puercos.  
+                <br>-Machaca casera.      
+                <br>-Queso Chihuahua.
+                <br>-Macaha casera.
+                <br>-Condimentos.
+                <br>-Tortilla de harina.
+            </p>
+            <p class="card-text"><strong>Precio: $40</strong></p>
+                <div class="d-grid gap-1">
+                    <button class="btn btn-success" type="button">Ordenar</button>
+                </div>
+        </div>
+    </div>
+    </div>
+</div>  
+
+<div class="banner">
+    <div class="contenido">
+    <h2>Los mejores Burritos</h2>
+    <h3>de la zona</h3>
+    <p>Pre-ordena tu pedido, ya que contamos con cantidades limitadas.
+        <br>
+        Santo Burrito Tiene una preparación 100% artesanal y con un toque secreto que hacen que sean extremadamente ricos.<br><strong> ¡No te quedes sin los tuyos y ordena tus burritos!</strong>
+    </p>
+    </div>
+    
+</div>
+
+<div class="banner-pago">
+    <div class="contenido">
+        <h1>Aceptamos trasferencias.</h1>
+        <h2>Número de cuenta</h2>
+        <h3>5204165186088393</h3>
+    </div>
+</div>
+    
+<?php include ('include/footer.php');?>
+
